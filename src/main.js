17e0e37a458e5ba31cd972231e72e8ef6aab85f5
v.alefman <v.alefman@superdao.co>
2022-12-18 15:18:47 +0200

    set first folders

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { getRandomInteger, getRandomElement } from "./util/random"
+
+for(let i=0; i<10; i++){
+    console.log(getRandomInteger(0,10))
+}
+
+console.log(getRandomElement(["abc", "lnn", "Vasya"]))
